8eb97d4904c22f49a4d1681ddf03e2e3b34ccc96
Caroline Araujo <ca.araujosouza@gmail.com>
2020-10-20 10:34:07 -0300

    first commit

---
+++ src/TestaVariaveis.java
@@ -0,0 +1,22 @@
+
+public class TestaVariaveis {
+
+	public static void main(String[] args) {
+		System.out.println("ola novo teste");
+		
+		int idade;
+		idade = 27;
+		
+		System.out.println(idade);
+		
+		idade = 27 + 10;
+		
+		System.out.println(idade);
+		
+		idade = (7 * 5) + 2;
+		
+		System.out.println(idade);
+		
+		System.out.println("a idade é " + idade + ", parabéns!");
+	}
+}
